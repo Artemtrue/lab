0c942943048135f0bf18ad15c704fb2d83a0e778
Artemtrue <artem-tishchenko-2002@mail.ru>
2021-10-30 11:08:22 +0300

    LaterInYear

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cassert>
 
 bool IsLeapYear(int year)
 {
@@ -6,6 +7,16 @@ bool IsLeapYear(int year)
 	return false;
 }
 
+//Напишите функцию LaterInYear, проверяющую, какой из двух дней ближе к 
+//Новому году (31.12, грядущему, а не прошедшему). Сделайте коммит.
+int LaterInYear(int x1, int x2)
+{
+	assert(x1 < 32 && x1 > 0);
+	assert(x2 < 32 && x2 > 0);
+	if (x2 > x1) return x2;
+	else return x1;
+
+}
 
 int main()
 {
